84ab4d84cb64c02d7d3ce024955da4dec8e84921
paul.rutledge <paul.rutledge@vodori.com>
2018-02-11 21:16:49 -0600

    add particles

--- src/cljs/typershark/game.cljs
+++ src/cljs/typershark/game.cljs
@@ -7,26 +7,54 @@
 
 (goog-define BASE_WEBSOCKET "ws://localhost:8090")
 
-(def STATE (atom {}))
+(def STATE (atom {:particles []}))
 (def GAME (atom nil))
+
+(defn get-boundaries []
+  [(quot (.-innerWidth js/window) 2)
+   (quot (.-innerHeight js/window) 2)])
+
+(defn new-particle [[width height]]
+  {:x       (* (rand) width)
+   :y       (+ height (* (rand) height))
+   :speed   (rand)
+   :radius  (+ 3 (* 6 (rand)))
+   :opacity (int (+ 150 (* 55 (rand))))})
+
+(defn particle->arc [{:keys [x y radius opacity]}]
+  [:fill
+   {:colors [255 255 255 opacity]}
+   [:ellipse {:x x :y y :width radius :height radius}]])
+
+(defn update-particle [{:keys [y speed] :as particle}]
+  (if (> -10 y)
+    (new-particle (get-boundaries))
+    (assoc particle :y (- y speed))))
 
 (def main-screen
   (reify play/Screen
 
     (on-show [this]
-      (println "Showing"))
+      (let [bounds    (get-boundaries)
+            renderer  (play/get-renderer @GAME)
+            particles (repeatedly 75 (partial new-particle bounds))]
+        (.blendMode renderer "lighter")
+        (swap! STATE assoc :particles particles)))
 
     (on-hide [this]
       (println "Hiding"))
 
     (on-render [this]
-      (play/render @GAME
-        []))))
+      (let [game  @GAME
+            state @STATE
+            parts (:particles state)]
+        (play/render game (mapv particle->arc parts))
+        (let [updated-particles (mapv update-particle parts)]
+          (swap! STATE assoc :particles updated-particles))))))
 
 (defn init-screen! []
-  (let [width  (quot (.-innerWidth js/window) 2)
-        height (quot (.-innerHeight js/window) 2)
-        game   (play/create-game width height)]
+  (let [[width height] (get-boundaries)
+        game (play/create-game width height)]
     (reset! GAME game)
     (doto game
       (play/start)
